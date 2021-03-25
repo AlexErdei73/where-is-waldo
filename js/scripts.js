c6098f0a6443ed7b1b2dbea6fbfae8a21c184894
Alex Erdei <mralexerdei@yahoo.co.uk>
2021-03-25 21:41:00 +0000

    add start button to the app

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,41 @@
 const image = document.querySelector("img");
 const popupMenu = document.querySelector(".popup-menu");
 const menuButtons = document.querySelectorAll(".choice");
+const startButton = document.querySelector("#start");
 const borderWidth = 30;
 const borderHeight = 60;
+let hasImageClicked = false;
+const coordinates = {
+  x: 0,
+  y: 0,
+};
 
 image.addEventListener("click", onClickImg);
 menuButtons.forEach((button) => {
   button.addEventListener("click", onClickBtn);
 });
+startButton.addEventListener("click", onClickStart);
 
 function onClickImg(event) {
+  if (hasImageClicked) return;
   const x = event.layerX;
   const y = event.layerY;
   console.log({ x, y });
+  coordinates.x = x;
+  coordinates.y = y;
   popupMenu.classList.add("show");
   popupMenu.style.top = `${y - borderHeight / 2}px`;
   popupMenu.style.left = `${x - borderWidth / 2}px`;
+  hasImageClicked = true;
 }
 
 function onClickBtn(event) {
   const target = event.target;
   console.log(target.textContent);
   popupMenu.classList.remove("show");
+  hasImageClicked = false;
+}
+
+function onClickStart() {
+  image.classList.add("show");
 }
