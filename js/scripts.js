aa3e1d3bed4b81ffff33b5327ac2b97023f66c68
Alex Erdei <mralexerdei@yahoo.co.uk>
2021-03-25 20:22:50 +0000

    add event listener to menu buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,25 @@
-const image = querrySelector("img");
+const image = document.querySelector("img");
+const popupMenu = document.querySelector(".popup-menu");
+const menuButtons = document.querySelectorAll(".choice");
+const borderWidth = 30;
+const borderHeight = 60;
 
-image.addEventListener("click", onClick);
+image.addEventListener("click", onClickImg);
+menuButtons.forEach((button) => {
+  button.addEventListener("click", onClickBtn);
+});
 
-function onClick(event) {
-  console.log(event);
+function onClickImg(event) {
+  const x = event.layerX;
+  const y = event.layerY;
+  console.log({ x, y });
+  popupMenu.classList.add("show");
+  popupMenu.style.top = `${y - borderHeight / 2}px`;
+  popupMenu.style.left = `${x - borderWidth / 2}px`;
+}
+
+function onClickBtn(event) {
+  const target = event.target;
+  console.log(target.textContent);
+  popupMenu.classList.remove("show");
 }
