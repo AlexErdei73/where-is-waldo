ecb0379ee76442cbde55b239b3fd59c2584e51ac
Alex Erdei <mralexerdei@yahoo.co.uk>
2021-04-09 16:28:43 +0100

    add itro page to the app

--- js/scripts.js
+++ js/scripts.js
@@ -14,7 +14,7 @@ const click = {
 };
 let gameID;
 const storageRef = storage.ref();
-const pictureIndex = 1;
+const pictureIndex = 0;
 const shiftRight = (() => {
   const marginLeft = getComputedStyle(container)["margin-left"];
   const indexOfPx = marginLeft.indexOf('px');
@@ -22,11 +22,7 @@ const shiftRight = (() => {
 })();
 const shiftDown = 60;
 container.style.marginTop = `${shiftDown}px`;
-const introduction = `Your task is to click on the picture to find Waldo, Odlaw and the
-Wizzard. If you ready to start press the button!`
-const pElement = document.createElement('p');
-pElement.textContent = introduction;
-container.appendChild(pElement);
+createIntroPage();
 
 image.addEventListener("click", onClickImg);
 startButton.addEventListener("click", onClickStart);
@@ -71,7 +67,7 @@ function onClickStart() {
     'pictures/waldo-2.jpg',
   ]
   startButton.style.visibility = 'hidden';
-  pElement.remove();
+  destroyIntroPage();
   storageRef.child(waldoPictures[pictureIndex]).getDownloadURL()
     .then((url) => {
       image.src = url;
@@ -157,4 +153,67 @@ function handleGameOver(data) {
   if (!time) return
   console.log(`GAME OVER, time = ${time}s`);
   gameOver = true;
+}
+
+function createIntroPage() {
+  const introduction1 = `Let me itroduce you Waldo and his friends:`
+  const pElement1 = document.createElement('p');
+  pElement1.textContent = introduction1;
+  pElement1.style.fontSize = '21px';
+  pElement1.style.width = '600px';
+  pElement1.style.margin = '20px auto';
+  container.appendChild(pElement1);
+  const figures = [
+    'assets/waldo.jpg',
+    'assets/odlaw.jpg',
+    'assets/wizzard.jpeg',
+    'assets/wenda.jpeg',
+  ];
+  const names = [
+    'waldo',
+    'odlaw',
+    'wizzard',
+    'wenda',
+  ]
+  let imgElement;
+  const div = document.createElement('div');
+  div.style.width = '600px';
+  div.style.margin = 'auto';
+  div.id = 'characters';
+  container.appendChild(div);
+  for (let i = 0; i < 4; i++) {
+    imgElement = document.createElement('img');
+    imgElement.src = figures[i];
+    imgElement.style.width = '50px';
+    imgElement.style.opacity = 1;
+    imgElement.style.margin = '20px';
+    div.appendChild(imgElement);
+  }
+  emptyDiv = document.createElement('div');
+  div.appendChild(emptyDiv);
+  for (let i = 0; i < 4; i++) {
+    nameElement = document.createElement('div');
+    nameElement.textContent = names[i];
+    nameElement.style.width = '50px';
+    nameElement.style.display = 'inline';
+    nameElement.style.margin = '20px';
+    div.appendChild(nameElement);
+  }
+  const introduction2 = `Your task is finding them on the pictures by 
+  clicking the right place on the picture. The program measures your
+  time, the faster you can find them, the better. Are you ready? 
+  Press the button!`
+  const pElement2 = document.createElement('p');
+  pElement2.textContent = introduction2;
+  pElement2.style.fontSize = '21px';
+  pElement2.style.width = '600px';
+  pElement2.style.margin = '20px auto';
+  container.appendChild(pElement2);
+}
+
+function destroyIntroPage() {
+  const pElements = container.querySelectorAll('p');
+  pElements.forEach(item => item.remove());
+  const div = container.querySelector('#characters');
+  div.remove();
 }
